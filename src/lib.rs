8af5a5cfa4ce4e59681cc41fbb5e3eb1cc64bdaf
Ben Batha <bhbatha@gmail.com>
2017-05-11 15:15:48 -0400

    VTree experiments

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,91 @@
-use std::collections::BTreeMap;
+extern crate id_tree;
 
-#[derive(Hash, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
-pub struct Div {
-    props: BTreeMap<String, Vec<String>>,
-}
+use id_tree::{Tree, Node, NodeId, NodeIdError, InsertBehavior};
 
-impl Div {
-    pub fn render(&self) -> String {
-        let mut div = "<div ".to_string();
-        for (name, values) in self.props.iter() {
-            div.push_str(name.as_str());
-            div.push_str("=\"");
-            div.push_str(&values.join(","));
-            div.push('"');
-        }
-        div.push_str("/>");
-        div
+struct VNode {
+    tag: String,
+    children: Vec<VNode>,
+};
+
+impl VNode {
+    fn new_root() -> VNode {
+        VNode()
     }
 }
 
-#[test]
-fn div_renders() {
-    let mut props = BTreeMap::new();
-    props.insert("class".to_string(), vec!["cool".to_string(), "story".to_string()]);
-    let d = Div { props };
-    assert_eq!(&d.render(), r#"<div class="cool,story"/>"#);
+trait Render {
+    fn render(&self, tree: &mut VTree);
+}
+
+
+
+impl VTree {
+    fn new() -> VTree { 
+        let mut tree = Tree::new();
+        let root = VNode::new_root();
+        let root_id = tree.insert(Node::new(root), InsertBehavior::AsRoot).expect("fresh");
+        VTree {
+            tree: tree,
+            cursor: root_id.clone()
+         }
+    }
+
+    fn insert_vnode(&mut self, vnode: VNode) -> Result<NodeId, NodeIdError> {
+        self.tree.insert(Node::new(vnode), InsertBehavior::UnderNode(&self.cursor))
+    }
+
+    fn insert_children<R: Render>(&mut self, parent: NodeId, children: &[R]) {
+        let prev_cursor = self.cursor.clone();
+        self.cursor = parent;
+
+        for renderable in children {
+            renderable.render(self);
+        }
+
+        self.cursor = prev_cursor;
+    }
+
+    fn create_patch_set(&self, old_tree: &VTree) {
+    }
+}
+
+impl Div {
+    fn new<R: Render>(children: &[R]) {
+    }
+    
+    fn render() -> VNode {
+        let childVNodes = Vec::new();
+        
+        for child in this.children {
+            childVNodes.push(generateTree(child));
+        }
+
+        return VNode {
+            tag: 'div',
+            children: childVNodes,
+        };
+    }
+}
+
+
+impl Component {
+    fn render() -> VNode {
+        div = Div::new();
+        child = OtherComponent::new();
+        div.appendChild(OtherComponent);
+        return div.render();
+
+
+        div.appendChild(OtherComponent.render());
+    }
+}
+
+let lastTree = EMPTY_TREE;
+loop {
+    let component = MyComponent::new(currentState);
+    h(component)
+    let newTree = MyComponent;
+    let patchSet = patch(lastTree, newTree);
+    applyPatch(patchSet, domElement);
+    lastTree = newTee;
 }
