fdbce37d939234eb4ba30c09a533432fe9079781
Ben Batha <bhbatha@gmail.com>
2017-05-11 23:29:59 -0400

    refactor Component to take a document and return NodeId

--- src/component.rs
+++ src/component.rs
@@ -1,17 +1,17 @@
-use std::hash;
-use vnode::VNode;
+use document::{VDocument, NodeId};
 
 pub trait Component {
-    fn render(&self) -> VNode;
+    fn render(&self, doc: &mut VDocument) -> NodeId;
 }
 
 impl Component for &'static str {
-    fn render(&self) -> VNode {
-        VNode::new_text(self.to_string())
+    fn render(&self, doc: &mut VDocument) -> NodeId {
+        doc.create_text_node(self)
     }
 }
 
-#[derive(Default, Eq, PartialEq)]
+/// HTML Div element
+#[derive(Debug, Default, Eq, PartialEq)]
 pub struct Div<C> {
     children: Vec<C>
 }
@@ -31,38 +31,24 @@ impl<C: Component> Div<C> {
 }
 
 impl<C: Component> Component for Div<C> {
-    fn render(&self, document) -> VNode {
-        let div_root = document.new_element("div");
-        let child = self.child.render(document);
-
-        document.append(child, div_root);
-
-        return div_root;
+    fn render(&self, doc: &mut VDocument) -> NodeId {
+        let div_id = doc.new_element("div");
+        for child in self.children.iter() {
+            let child_id = child.render();
+            doc.append_child(div_id, child_id);
+        }
+        div_id
     }
 }
 
-impl Component for () {
-    fn render(&self) -> VNode {
-        VNode::new_empty()
-    }
-}
-
+/// Use this type to compose a component and assign it a user friendly Id key
+#[derive(Debug, Default, Eq, PartialEq)]
 pub struct KeyedComponent<K, C>(K, C);
 
 impl<K: hash::Hash, C: Component> Component for KeyedComponent<K, C> {
-    fn render(&self) -> VNode {
-        let mut vnode = self.1.render();
-        vnode.set_key(&self.0);
-        vnode
+    fn render(&self, doc: &mut VDocument) -> NodeId {
+        let id = self.1.render();
+        doc.set_key(&self.0, id);
+        id
     }
-}
-
-#[test]
-fn render_keyed() {
-    let key = 3;
-    let div: Div<()> = Div::new();
-    let keyed = KeyedComponent(key, div);
-    let mut actual = VNode::new_element("div", Vec::new());
-    actual.set_key(&key);
-    assert_eq!(actual.key, keyed.render().key);
 }
