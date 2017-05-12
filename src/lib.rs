3bafd1200904ae408f4a526ed011af4b3e49695d
Ben Batha <bhbatha@gmail.com>
2017-05-11 21:00:54 -0400

    support anonymous keys

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,70 @@
+extern crate ordermap;
+extern crate snowflake;
+
+use std::hash;
+
+#[derive(Debug, Eq)]
+pub struct VNode {
+    ty: VNodeType,
+    key: u64,
+} 
+
 #[derive(Debug, Eq, PartialEq)]
-pub enum VNode {
+enum VNodeType {
     Element {
         tag: &'static str,
-        children: Vec<VNode>,
-        key: Option<Key>,
+        children: ordermap::OrderMap<u64, VNode>,
     },
     Text(String),
     Empty,
 }
 
+impl hash::Hash for VNode {
+    fn hash<H: hash::Hasher>(&self, state: &mut H) {
+        self.key.hash(state)
+    }
+}
+
+impl PartialEq for VNode {
+    fn eq(&self, other: &VNode) -> bool {
+        self.key == other.key
+    }
+}
+
 impl VNode {
-    fn set_key(&mut self, key: Key) {
-        match *self {
-            VNode::Element{ key: ref mut k, .. } => *k = Some(key),
-            _ => (),
-        }
+    fn new_text<I: Into<String>>(text: I) -> VNode {
+        let mut vn = VNode {
+            key: 0,
+            ty: VNodeType::Text(text.into()),
+        };
+        vn.set_key(&snowflake::ProcessUniqueId::new());
+        vn
+    }
+
+    fn new_element<I: std::iter::IntoIterator<Item=VNode>>(tag: &'static str, children: I) -> VNode {
+        let children = children.into_iter().map(|n| (n.key, n)).collect();
+        let mut vn: VNode = VNode {
+            key: 0,
+            ty: VNodeType::Element{ tag, children }
+        };
+        vn.set_key(&snowflake::ProcessUniqueId::new());
+        vn
+    }
+
+    fn new_empty() -> VNode {
+        let mut vn = VNode {
+            key: 0,
+            ty: VNodeType::Empty,
+        };
+        vn.set_key(&snowflake::ProcessUniqueId::new());
+        vn
+    }
+
+    fn set_key<H: hash::Hash>(&mut self, key: &H) {
+        use std::hash::Hasher;
+        let mut hasher = std::collections::hash_map::DefaultHasher::new();
+        key.hash(&mut hasher);
+        self.key = hasher.finish();
     }
 }
 
@@ -24,7 +74,7 @@ pub trait Component {
 
 impl Component for &'static str {
     fn render(&self) -> VNode {
-        VNode::Text(self.to_string())
+        VNode::new_text(self.to_string())
     }
 }
 
@@ -49,48 +99,32 @@ impl<C: Component> Div<C> {
 
 impl<C: Component> Component for Div<C> {
     fn render(&self) -> VNode {
-        VNode::Element {
-            tag: "div",
-            children: self.children.iter().map(Component::render).collect(),
-            key: None,
-        }
+        VNode::new_element("div", self.children.iter().map(Component::render))
     }
 }
 
 impl Component for () {
     fn render(&self) -> VNode {
-        VNode::Empty
+        VNode::new_empty()
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Default, Hash, Clone)]
-pub struct Key(i32);
+pub struct KeyedComponent<K, C>(K, C);
 
-pub struct KeyedComponent<C>(Key, C);
-
-impl<C: Component> Component for KeyedComponent<C> {
+impl<K: hash::Hash, C: Component> Component for KeyedComponent<K, C> {
     fn render(&self) -> VNode {
         let mut vnode = self.1.render();
-        vnode.set_key(self.0.clone());
+        vnode.set_key(&self.0);
         vnode
     }
 }
 
 #[test]
-fn render_div() {
-    let div: Div<()> = Div::new();
-    assert_eq!(VNode::Element{ tag: "div", children: vec![], key: None}, div.render());
-}
-
-#[test]
-fn render_text_div() {
-    let div = Div::with_children(vec!["test"]);
-    assert_eq!(VNode::Element{ tag: "div", children: vec![VNode::Text("test".to_string())], key: None}, div.render())
-}
-
-#[test]
 fn render_keyed() {
+    let key = 3;
     let div: Div<()> = Div::new();
-    let keyed = KeyedComponent(Key(3), div);
-    assert_eq!(VNode::Element{ tag: "div", children: vec![], key: Some(Key(3))}, keyed.render());
+    let keyed = KeyedComponent(key, div);
+    let mut actual = VNode::new_element("div", Vec::new());
+    actual.set_key(&key);
+    assert_eq!(actual, keyed.render());
 }
